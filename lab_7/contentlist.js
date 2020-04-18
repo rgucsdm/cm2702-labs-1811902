4da494820ac2212fb0956cb45f4a3c473afd6eae
Eleonora Kapsule <1811902@rgu.ac.uk>
2020-04-18 22:49:34 +0000

    Events

--- lab_7/contentlist.js
+++ lab_7/contentlist.js
@@ -14,6 +14,35 @@ function addContent () {
 	// using javascript
 	// 1. find the content div
 	// 2. modify its html attribute by adding items_html
+	document.getElementById("content").innerHTML += items_html;
+}
 
+function addMoreContent() {
+    var items = ["hewey", "dewey", "louie"];
+    
+    // create new ul element
+    var ul = document.createElement("ul");
+    
+    // add list item as a child to <ul>
+    for (var i=0; i < items.length; i++) {
+        var li = document.createElement("li");
+        li.innerHTML = items[i];
+        ul.appendChild(li);
+	};
+
+    // add ul to content
+    document.getElementById("content").appendChild(ul);
+}
+
+function addItem(form) {
+    var newItem = form.newItem.value;
+    var li = document.createElement("li");
+    li.innerHTML = newItem;
+    document.getElementById("customList").appendChild(li);
+}
+
+function removeItem() {
+    var customList = document.getElementById("customList");
+    customList.removeChild(customList.lastChild);
 }
 
