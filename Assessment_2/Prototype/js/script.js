99c382618b7347ef648236228fa126ae96f23dd2
Eleonora Kapsule <1811902@rgu.ac.uk>
2020-07-14 11:45:07 +0000

    Assessment 2

--- Assessment_2/Prototype/js/script.js
+++ Assessment_2/Prototype/js/script.js
@@ -1,12 +1,33 @@
-$(function(){
+$(function() {
   
   /* PREDICTIONS */
   
-  /* Assign events to buttons */
+  /* Buttons - predict match outcome */
+  $(".btn-group>button").click(function() {
+    var btnClicked = event.target;
+    var matchID = $(this).parent().parent().attr("id");
+    $(btnClicked).addClass("selected");
+  
+    $("#"+matchID+">.btn-group").children().each(function() {
+      if(this.name != btnClicked.name) {
+        $(this).removeClass("selected");
+      }
+    });
+    console.log("clicked " + btnClicked.name + " in " + matchID);
+    displayDraw(btnClicked, matchID);
+  });
   
   /* Display select winner option when DRAW is selected */
+  function displayDraw(btnClicked, matchID) {
+    if(btnClicked.name == "Draw") {
+      //var matchID = $(btnClicked).parent().parent().attr("id");
+      $("#"+ matchID + ">select").css("visibility", "visible");
+      console.log("you said it's a draw in " + matchID);
+    }
+    else {
+      $("#"+ matchID + ">select").css("visibility", "hidden");
+    }; 
+  }
   
-  
-  
-  
+
 }); /* end of document ready */
