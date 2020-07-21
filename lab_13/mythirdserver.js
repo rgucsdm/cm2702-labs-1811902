d31f975b3da2cc228869b9b34b0fc4e3b8dc1d06
Eleonora Kapsule <1811902@rgu.ac.uk>
2020-07-21 15:03:43 +0000

    Lab 13

--- lab_13/mythirdserver.js
+++ lab_13/mythirdserver.js
@@ -1,6 +1,8 @@
 var http = require("http");
+var knockknock = require("knock-knock-jokes");
 
 http.createServer(function(req, res) {
   res.writeHead(200, {"Content-Type": "text/html"});
-  res.end("Hello World!");
+  var randomJoke = knockknock();
+  res.end(randomJoke);
 }).listen(8080);
