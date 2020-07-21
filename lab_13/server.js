532cc53efb8740560df5c0507bd00f071196512f
Eleonora Kapsule <1811902@rgu.ac.uk>
2020-07-21 15:24:57 +0000

    Lab 13

---
+++ lab_13/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+/* Route 1 */
+app.get("/", function(req, res) {
+  res.send("Hello world! by express");
+});
+
+app.listen(8080);
+
+/* Route 2 */
+app.get("/test", function(req, res) {
+  res.send("Hello world! by express - via Route 2");
+});
