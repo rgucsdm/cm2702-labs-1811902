d6bd1b9385cac230c6f4649142610fe726831704
Eleonora Kapsule <1811902@rgu.ac.uk>
2020-07-22 10:13:32 +0000

    Lab 13

--- lab_13/server.js
+++ lab_13/server.js
@@ -41,3 +41,10 @@ app.get("/calc", function(req, res){
   if(op=="div") {result = x/y};
   res.send("Result is "+ result);
 });
+
+/* Form */
+app.get("/getform", function (req, res) {
+  var name = req.query.name;
+  var quest = req.query.quest;
+  res.send("Hi " + name + ", I am sure you will "+ quest);
+});
