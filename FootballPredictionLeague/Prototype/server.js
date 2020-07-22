123c1fd27dcd6e5d7348425465c9e31730a3d5da
Eleonora Kapsule <1811902@rgu.ac.uk>
2020-07-22 10:59:37 +0000

    Assessment 3

--- FootballPredictionLeague/Prototype/server.js
+++ FootballPredictionLeague/Prototype/server.js
@@ -2,5 +2,9 @@ var express = require("express");
 var app = express();
 app.use(express.static("public"));
 
+app.get('/', function(req, res){
+  res.send("Hello world! by express”);
+});
+
 app.listen(8080);
 console.log("Your server is listening for requests on port 8080");
