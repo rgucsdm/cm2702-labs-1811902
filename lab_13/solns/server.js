6f89db212e44c7359d1d69401ace5e0fe39eee20
Eleonora Kapsule <1811902@rgu.ac.uk>
2020-07-22 10:20:49 +0000

    Lab 13

--- lab_13/solns/server.js
+++ lab_13/solns/server.js
@@ -37,7 +37,7 @@ app.get('/calc', function(req, res){
    res.send(retn);
 });
 
-app.get('/getform', function(req, res){
+app.get("/getform", function(req, res){
     var name = req.query.name;
     var quest = req.query.quest;
     res.send("Hi "+name+" I am sure you will find out "+quest) ;
