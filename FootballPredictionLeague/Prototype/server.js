37222d5c386ef33c5e4ecd2befd23acbffb7768e
Eleonora Kapsule <1811902@rgu.ac.uk>
2020-07-26 19:49:15 +0000

    Assessment 3

--- FootballPredictionLeague/Prototype/server.js
+++ FootballPredictionLeague/Prototype/server.js
@@ -42,7 +42,8 @@ app.get("/predictions", function (req, res) {
   var match51 = "TBC";
   db.collection("wc2018").find().toArray(function(err, result) {
     if(err) throw err;
-    match51 = $.grep(result, function(e){return e.matchID == "match51"});
+    //match51 = $.grep(result, function(e){return e.matchID == "match51"});
+    match51 = result.find(e => e.matchID === "match51");
     console.log(match51);
     })
     //console.log("Getting there");
